8f804a59fa9de92bff090c2e3789b9d05311c741
email <jackvarkees@gmail.con>
2024-05-11 22:32:57 +0530

    jack

---
+++ index.js
@@ -0,0 +1,23 @@
+// var jack="12 13 14 15 16 17 28";
+// let lio =jack.split(" ").map(Number);
+// console.log(lio);
+
+// var input0="12 13 14 15 16 17 18 "
+// var input =input0.split(" ").map(Number);
+// for(let i=0;i<input.length;i++){
+//     if(input[i]%2!=0){
+//         console.log(input[i])
+//     }
+// }
+
+// var input="guvi";
+// var guvi=input.split("").join(' ');
+// console.log(guvi);
+
+// var input="348".split("").join(" ");
+// console.log(input);
+
+// var input="33.3333";
+
+// var nextstep=+input ;
+// console.log(nextstep.toFixed(2));
